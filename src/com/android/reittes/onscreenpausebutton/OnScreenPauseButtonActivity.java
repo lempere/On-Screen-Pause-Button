4a50f4563e21982eba18842ea1d2d76ed376f4d6
guillem.perez <guillem@ubuntu.(none)>
2012-05-18 10:08:44 +0200

    Work with SimpleBaseActivity

--- src/com/android/reittes/onscreenpausebutton/OnScreenPauseButtonActivity.java
+++ src/com/android/reittes/onscreenpausebutton/OnScreenPauseButtonActivity.java
@@ -1,30 +1,31 @@
 package com.android.reittes.onscreenpausebutton;
 
-import org.anddev.andengine.engine.Engine;
-import org.anddev.andengine.engine.camera.Camera;
-import org.anddev.andengine.engine.options.EngineOptions;
-import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
-import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
-import org.anddev.andengine.entity.modifier.LoopEntityModifier;
-import org.anddev.andengine.entity.modifier.PathModifier;
-import org.anddev.andengine.entity.modifier.PathModifier.Path;
-import org.anddev.andengine.entity.modifier.SequenceEntityModifier;
-import org.anddev.andengine.entity.primitive.Rectangle;
-import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.scene.menu.MenuScene;
-import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
-import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
-import org.anddev.andengine.entity.scene.menu.item.SpriteMenuItem;
-import org.anddev.andengine.entity.shape.Shape;
-import org.anddev.andengine.entity.sprite.Sprite;
-import org.anddev.andengine.entity.util.FPSLogger;
-import org.anddev.andengine.input.touch.TouchEvent;
-import org.anddev.andengine.opengl.texture.TextureOptions;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
-import org.anddev.andengine.opengl.texture.region.TextureRegion;
-import org.anddev.andengine.ui.activity.BaseGameActivity;
-import org.anddev.andengine.util.modifier.ease.EaseSineInOut;
+
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FixedResolutionPolicy;
+import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.PathModifier;
+import org.andengine.entity.modifier.SequenceEntityModifier;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.andengine.entity.scene.menu.item.IMenuItem;
+import org.andengine.entity.scene.menu.item.SpriteMenuItem;
+import org.andengine.entity.shape.Shape;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.modifier.ease.EaseSineInOut;
+import org.andengine.entity.modifier.PathModifier.Path;
 
 import android.view.MotionEvent;
 
@@ -39,7 +40,7 @@ import android.view.MotionEvent;
  * @author Reittes
  */
 
-public class OnScreenPauseButtonActivity extends BaseGameActivity implements IOnMenuItemClickListener{
+public class OnScreenPauseButtonActivity extends SimpleBaseGameActivity implements IOnMenuItemClickListener{
 
 	private Scene scene;
 	private Camera camera;
@@ -48,64 +49,16 @@ public class OnScreenPauseButtonActivity extends BaseGameActivity implements IOn
 	private float CAMERA_WIDTH= 800;
 	
 	private BitmapTextureAtlas btnTexture;
-	private TextureRegion btn_pauseRegion, btn_playRegion;
+	ITextureRegion btn_pauseRegion;
+	private ITextureRegion btn_playRegion;
 	private Sprite pauseBtn;
 
-	
-	@Override
-	public Engine onLoadEngine() {
-		camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
-		engineOptions.getTouchOptions().setRunOnUpdateThread(true);
-		engineOptions.getRenderOptions().disableExtensionVertexBufferObjects();
-		return new Engine(engineOptions);
-	}
-
-	@Override
-	public void onLoadResources() {
-		btnTexture= new BitmapTextureAtlas(64,32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		btn_pauseRegion= BitmapTextureAtlasTextureRegionFactory.createFromAsset(btnTexture, this, "gfx/pause.png", 0 ,0);
-		btn_playRegion= BitmapTextureAtlasTextureRegionFactory.createFromAsset(btnTexture, this, "gfx/play.png", 32 ,0);
-		
-		getEngine().getTextureManager().loadTexture(btnTexture);
-	}
-
-	@Override
-	public Scene onLoadScene() {
-		getEngine().registerUpdateHandler(new FPSLogger());
-		
-		scene= new Scene();
-				
-		final Shape rectSprite= new Rectangle(200, 230, 50, 50);
-		rectSprite.setColor(1, 1, 1);
-		rectSprite.registerEntityModifier(new LoopEntityModifier(new SequenceEntityModifier(
-				new PathModifier(0.9f, new Path(2).to(200, 230).to(400,230), EaseSineInOut.getInstance()),
-				new PathModifier(0.9f, new Path(2).to(400, 230).to(200,230), EaseSineInOut.getInstance())
-				)));
-		scene.attachChild(rectSprite);
-		
-		pauseBtn= new Sprite(CAMERA_WIDTH- this.btn_pauseRegion.getWidth()-100, 50, this.btn_pauseRegion){
-			@Override
-			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-	   			 if(pSceneTouchEvent.getAction()==MotionEvent.ACTION_UP){
-	   				 this.setVisible(false);
-	   				 scene.setChildScene(pauseScene(), false, true, true);
-	   			 }
-	   			 return true;
-       	}};
-       	pauseBtn.setScale(2);
-       	scene.attachChild(pauseBtn);
-       	scene.registerTouchArea(pauseBtn);
-		
-		return scene;
-	}
-	
 	// Menu Scene with Play Button 
 	
 	private MenuScene pauseScene(){
 		final MenuScene pauseGame= new MenuScene(camera);
 		
-		final SpriteMenuItem btnPlay = new SpriteMenuItem(1, this.btn_playRegion);
+		final SpriteMenuItem btnPlay = new SpriteMenuItem(1, this.btn_playRegion, getVertexBufferObjectManager());
 		btnPlay.setPosition(CAMERA_WIDTH- this.btn_pauseRegion.getWidth()-100, 50);
 		btnPlay.setScale(2);
 		pauseGame.addMenuItem(btnPlay);
@@ -114,11 +67,7 @@ public class OnScreenPauseButtonActivity extends BaseGameActivity implements IOn
 		pauseGame.setOnMenuItemClickListener(this);
 		return pauseGame;
 	}
-	@Override
-	public void onLoadComplete() {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 	@Override
 	public boolean onMenuItemClicked(MenuScene arg0, IMenuItem arg1,
@@ -133,6 +82,60 @@ public class OnScreenPauseButtonActivity extends BaseGameActivity implements IOn
 		default:
 			return false;
 		}
+	}
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		final EngineOptions eOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_SENSOR,
+				new FixedResolutionPolicy((int)CAMERA_WIDTH, (int)CAMERA_HEIGHT), camera);
+		return eOptions;
+	}
+
+	@Override
+	protected void onCreateResources() {
+		btnTexture= new BitmapTextureAtlas(this.getTextureManager(), 64,32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		btn_pauseRegion= BitmapTextureAtlasTextureRegionFactory.createFromAsset(btnTexture, this, "gfx/pause.png", 0 ,0);
+		btn_playRegion= BitmapTextureAtlasTextureRegionFactory.createFromAsset(btnTexture, this, "gfx/play.png", 32 ,0);
+		
+		getEngine().getTextureManager().loadTexture(btnTexture);
+		
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		this.mEngine.registerUpdateHandler((IUpdateHandler) new FPSLogger());
+		
+		scene= new Scene();
+				
+		final Shape rectSprite= new Rectangle(200, 230, 50, 50, getVertexBufferObjectManager());
+		rectSprite.setColor(1, 1, 1);
+		float[][] vx = {{200,230},{400,230},
+						{200,230},{400,230},
+						{400,230},{200,230}};
+
+		
+		rectSprite.registerEntityModifier(new LoopEntityModifier(new SequenceEntityModifier(
+				new PathModifier(0.8f, new Path(vx[0], vx[1]),  EaseSineInOut.getInstance()),
+				new PathModifier(0.9f, new Path(vx[2],vx[3]), EaseSineInOut.getInstance()),
+				new PathModifier(0.9f, new Path(vx[4],vx[5]), EaseSineInOut.getInstance())
+				)));
+		scene.attachChild(rectSprite);
+		
+		pauseBtn= new Sprite(CAMERA_WIDTH- this.btn_pauseRegion.getWidth()-100, 50, this.btn_pauseRegion, getVertexBufferObjectManager()){
+			@Override
+			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+	   			 if(pSceneTouchEvent.getAction()==MotionEvent.ACTION_UP){
+	   				 this.setVisible(false);
+	   				 scene.setChildScene(pauseScene(), false, true, true);
+	   			 }
+	   			 return true;
+       	}};
+       	pauseBtn.setScale(2);
+       	scene.attachChild(pauseBtn);
+       	scene.registerTouchArea(pauseBtn);
+		
+		return scene;
 	}
 
 	
